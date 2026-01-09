59b5944eb86bd8d0a4dc2a1bb8ba3fd7ff637b54
Christoph Stach <christoph.stach@gmail.com>
2026-01-09 11:56:18 +0100

    chore: integrate jsx-a11y plugin for improved accessibility linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import antfu from '@antfu/eslint-config'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 
 export default antfu({
   react: true,
   ignores: ['dist'],
-})
+}, jsxA11y.flatConfigs.recommended)
